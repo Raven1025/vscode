9cdaab0dfa0956fd93c5d8827597801341e39906
Raven1025 <leilei_1025@qq.com>
2018-03-13 11:53:51 +0800

    Seven_Parameter

--- Coordinate_Input.py
+++ Coordinate_Input.py
@@ -1,14 +1,25 @@
 #七参数的输入
-before=[]
-after=[]
-
-num=int(input("请输入坐标对的个数："))
-
-for counter in range(num):
-    befor.append([])
-      
-if __name__=="__main__":
-    for counter in range(int(num)):
-        print("请输入第%d个坐标：" % int(counter+1)
-        before_one=before(input())
+def Coordinate_Input(num):
     
+    before=[]
+    after=[]
+    
+    for i in range(num):
+        print("请输入第一坐标系下的第%s个坐标：" % str(i+1))
+	    temp=list(map(double,input().split()))
+	    before.append(temp)
+
+    for i in range(num):
+        print("请输入对应的第二坐标系下的第%s个坐标" % str(i+1))
+        temp=list(map(double,input().split()))
+        after.append(temp)
+
+    return(before,after)
+
+if __name__=="__main__":
+    num=int(input("请输入坐标对数："))
+
+    before=[]
+    after=[]
+    before,after=Coordinate_Input(num)
+    print(before,after)
