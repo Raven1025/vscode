b0b22f6675ade0823e472bc5541009b83651cd41
Raven1025 <leilei_1025@qq.com>
2018-04-10 11:22:18 +0800

    最新的input

--- Coordinate_Input.py
+++ Coordinate_Input.py
@@ -1,14 +1,30 @@
 #七参数的输入
-before=[]
-after=[]
+def Coordinate_Convert():
+    coordinate_before=[]
+    coordinate_after=[]
 
-num=int(input("请输入坐标对的个数："))
+    num=int(input("请输入坐标对的个数："))
 
-for counter in range(num):
-    befor.append([])
-      
-if __name__=="__main__":
-    for counter in range(int(num)):
-        print("请输入第%d个坐标：" % int(counter+1)
-        before_one=before(input())
+    for counter in range(num):
+        coordinate_before.append([])
+        coordinate_after.append([])
     
+    #if __name__=="__main__":
+    for counter in range(num):
+        temp=input('请输入坐标系1下的第%s个坐标：'% str(counter+1))
+        coordinate_before[counter]=list(map(float,temp.strip().split()))
+        print (coordinate_before[counter])
+
+    for counter in range(num):
+        temp=input('请输入坐标系2下的第%s个坐标：'% str(counter+1))
+        coordinate_after[counter]=list(map(float,temp.strip().split()))
+        print (coordinate_after[counter])
+        
+
+    print(coordinate_before)
+    print(coordinate_after)
+
+    return coordinate_before,coordinate_after
+
+if __name__=='__main__':
+    Coordinate_Convert()
